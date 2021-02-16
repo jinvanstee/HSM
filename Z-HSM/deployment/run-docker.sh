a6fca3e8f60071bb1cd88027a3d5a315e01187e4
Jin VanStee <jinxiong@us.ibm.com>
2021-02-16 16:33:41 -0500

    Update run-docker.sh
    Updated to include volume mount for key store directory, also updated image name to match the image built in build step

--- Z-HSM/deployment/run-docker.sh
+++ Z-HSM/deployment/run-docker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -ux
 
 docker run -it -d --name=pkcs11-proxy-opencryptoki \
-    -v /home/icpcrypto/hsm/token/opencryptoki:/var/lib/opencryptoki \
+    -v /var/lib/opencryptoki:/var/lib/opencryptoki \
+    -v /etc/opencryptoki:/etc/opencryptoki \
     --device=/dev/z90crypt:/dev/z90crypt \
     -e EP11_SLOT_NO=4 \
-    -e EP11_SLOT_TOKEN_LABEL=PKCS11 \
-    -e EP11_SLOT_SO_PIN=87654313 \
-    -e EP11_SLOT_USER_PIN=87654312 \
+    -e EP11_SLOT_TOKEN_LABEL=EP11Tok \
+    -e EP11_SLOT_SO_PIN=12345678 \
+    -e EP11_SLOT_USER_PIN=84959689 \
     -p 2345:2345 \
-    ibmcom/pkcs11-proxy-opencryptoki:s390x-1.0.0
+    pkcs11-proxy-opencryptoki:s390x-1.0.0
